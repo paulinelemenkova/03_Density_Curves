79bf13e146724be918a3c7246a0fb4e44cfdc002
Polina Lemenkova <pauline.lemenkova@gmail.com>
2025-10-23 21:20:31 +0300

    Add files via upload

--- Density_23102025.r
+++ Density_23102025.r
@@ -1,26 +1,6 @@
 library(ggplot2)
 df <- read.csv("FAOSTAT_India_12.csv")
 #head(df)
-# Basic histogram
-ggplot(df, aes(x=Value)) +
-    geom_histogram()
-# Change the width of bins
-ggplot(df, aes(x=weight)) +
-    geom_histogram(binwidth=1)
-# Change colors
-p<-ggplot(df, aes(x=weight)) +
-  geom_histogram(color="black", fill="white")
-p
-
-ggplot(df, aes(x=Value, fill=Item, color=Item)) +
-    geom_histogram(binwidth = 500, position="identity", alpha=0.5,)
-
-# Specify a function for calculating binwidth when faceting along variables with
-# different ranges: the function will be called once per facet
-ggplot(df, aes(Value, fill=Item, color=Item)) +
-    facet_wrap(~Item, scales = 'free_x') +
-    geom_histogram(binwidth = 100, linejoin = "round") +
-    geom_density(adjust=1.5)
 
 p2 <- ggplot(df, aes(Value, group=Item, fill=Item)) +
     geom_density(adjust=2.5, alpha=.4) +
